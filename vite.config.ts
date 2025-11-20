ad5327ce81c09ce12631ba3a7995d860489cf3a2
Johan Pérez <johanperezmarin@gmail.com>
2025-11-19 19:21:53 -0500

    Config Build for deploying to production for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/Portafolio/",
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
+
